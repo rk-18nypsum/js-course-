3c4231744cc5dc1fb899b69e7d011f1afc84b307
Rajeev Kumar <krajeev8033@gmail.com>
2026-02-10 07:03:14 +0000

    data types

---
+++ basics/modern.js
@@ -0,0 +1,16 @@
+"use strict" ; //treat all js code as newer verson 
+//alert (3=3)  // we are using  nodejs,not browser
+//coder readability should be high
+
+
+//data types:-
+let names = "rajeev"; //string
+
+let num = 56;//number
+let ans = true; //boolean
+let a =null; //standalone  like empty data type !=0
+//undefined;
+//symbol
+console.log(typeof a);
+console.log(typeof names);
+
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,15 @@
-console.log("hello");
+
+
+const account_Id = "rajeev";
+let email = "abc@gmail.com";
+var num = 88840;
+city = "lucknow"
+let count ;
+
+// const cannot be changed  further 
+console.log(account_Id);
+console.table ([account_Id,email,num,city,course = "b.tech",count]);
+
+/*  
+prefer not to use var due to block and functional scoped reason;
+*/
