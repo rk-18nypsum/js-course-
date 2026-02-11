73789f6e8de995bc49d6d5e7d0cd931b05bea1d3
Rajeev Kumar <krajeev8033@gmail.com>
2026-02-11 17:25:32 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,33 @@
+ // 1- primitive
+
+// 7 types :-> string, number ,boolean ,null, undefined, symbol
+   const score = 89;
+   let name = " rajeev";
+   let isNum = null;
+   var id = Symbol('23');
+   var id2 = Symbol('23');
+
+//    console.log(id === id2);
+   const bigNumber = 42358704456437995746n;
+
+// 2.Reference (non-pritive
+// array, objects, function
+
+const hii = ["virat","rahul","kl rahlul"]
+console.log(hii);
+
+
+const obj ={
+    name :"raj",
+    roll: 90,
+    price: 76
+}
+console.log( typeof(hii));
+ function Hii(){
+    console.log("hello")
+    
+}
+Hii();
+
+//js is dynakmically typed lang
+
